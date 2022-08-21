b35b70b5b09857cbbff3c3c7919e6821f640baa9
sarthakvish <sarthakvishwakarma6@gmail.com>
2022-08-21 15:15:49 +0530

    header team

--- src/Pages/OurTeam.js
+++ src/Pages/OurTeam.js
@@ -1,13 +1,55 @@
 import React from 'react';
-
+import { Card } from 'antd';
+import Meta from "antd/es/card/Meta";
+import { Col, Divider, Row } from 'antd';
+import "antd/dist/antd.css";
 
 const OurTeam = () => {
+    const style = {
+        background: '#0092ff',
+        padding: '8px 0',
+    };
     return (
-        <div>
-            <h1>OurTeam</h1>
+        <div style={{ background: '#ECECEC', padding: '30px' }}>
+            <Row gutter={16}>
+                <Col span={8}>
+                    <Card
+                        hoverable
+                        style={{
+                            width: 240,
+                        }}
+                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                    >
+                        <Meta title="Europe Street beat" description="www.instagram.com" />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card
+                        hoverable
+                        style={{
+                            width: 240,
+                        }}
+                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                    >
+                        <Meta title="Europe Street beat" description="www.instagram.com" />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card
+                        hoverable
+                        style={{
+                            width: 240,
+                        }}
+                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                    >
+                        <Meta title="Europe Street beat" description="www.instagram.com" />
+                    </Card>
+                </Col>
+            </Row>
         </div>
     );
-};
+}
+
 
 export default OurTeam;
 
